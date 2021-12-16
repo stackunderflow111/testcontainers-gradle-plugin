1dbf8f82baf8e426d22f890c9b9bfbe0b77a589c
Yao Zou <yao.zou@hulu.com>
2021-12-15 19:52:18 -0800

    plugin published

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,34 @@
 plugins {
-    `java-gradle-plugin`
+    id("java-gradle-plugin")
+    id("maven-publish")
+    id("com.gradle.plugin-publish") version "0.16.0"
 }
 
 group = "github.stackunderflow111"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation("org.testcontainers:jdbc:1.16.2")
+    implementation("org.flywaydb:flyway-core:8.2.2")
 }
 
 gradlePlugin {
     plugins {
         create("jooqTestcontainersPlugin") {
             id = "github.stackunderflow111.testcontainers"
+            displayName = "Testconatiners plugin"
+            description = "Configure an existing task to run testcontainers in its doFirst block"
             implementationClass = "github.stackunderflow111.TestcontainersPlugin"
         }
     }
 }
 
-dependencies {
-    implementation("org.testcontainers:jdbc:1.16.2")
-    implementation("org.flywaydb:flyway-core:8.2.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+pluginBundle {
+    website = "https://github.com/stackunderflow111/testcontainers-plugin"
+    vcsUrl = "https://github.com/stackunderflow111/testcontainers-plugin.git"
+    tags = listOf("testcontainers", "jooq", "flyway")
 }
