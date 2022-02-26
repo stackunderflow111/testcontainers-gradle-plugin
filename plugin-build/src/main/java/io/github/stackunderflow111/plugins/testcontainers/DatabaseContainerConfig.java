085e14884ac187a0ec9d423a2fb7731b932c961c
Yao Zou <yao.zou@hulu.com>
2022-02-26 00:39:33 -0800

    remove unused inject

--- plugin-build/src/main/java/io/github/stackunderflow111/plugins/testcontainers/DatabaseContainerConfig.java
+++ plugin-build/src/main/java/io/github/stackunderflow111/plugins/testcontainers/DatabaseContainerConfig.java
@@ -1,12 +1,10 @@
 package io.github.stackunderflow111.plugins.testcontainers;
 
-import javax.inject.Inject;
 import org.gradle.api.provider.Property;
 
 public abstract class DatabaseContainerConfig {
     private final String name;
 
-    @Inject
     public DatabaseContainerConfig(String name) {
         this.name = name;
     }
