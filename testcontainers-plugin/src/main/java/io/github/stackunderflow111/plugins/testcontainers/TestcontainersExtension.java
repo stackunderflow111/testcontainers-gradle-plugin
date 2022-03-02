8167cf7f552525b5dc5bc9d7208e7cd03c49a367
Yao Zou <yao.zou@hulu.com>
2022-03-01 22:49:39 -0800

    remove extension

--- testcontainers-plugin/src/main/java/io/github/stackunderflow111/plugins/testcontainers/TestcontainersExtension.java
+++
@@ -1,7 +0,0 @@
-package io.github.stackunderflow111.plugins.testcontainers;
-
-import org.gradle.api.NamedDomainObjectContainer;
-
-public interface TestcontainersExtension {
-    NamedDomainObjectContainer<DatabaseContainerConfig> getDatabaseContainerBuildServices();
-}
